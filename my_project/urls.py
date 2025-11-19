d99383e2df6ddb5a5de200031358be61b3374db4
HamzaRajpu456 <hamzarajput7896@gmail.com>
2025-11-19 19:36:59 +0500

    create templates folder and html template and then render into views at home page for testing and also declare a path of templates into settings.py file

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from product.views import home_view
+from product.views import home_view, contact_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name = 'home/'),
+    path('home/', home_view),
+    path('contact/', contact_view ),
+    path('about/', about_view),
+
 ]
 
