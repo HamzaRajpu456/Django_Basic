906f478676dfe9b8548e18cb861053c229cbc149
HamzaRajpu456 <hamzarajput7896@gmail.com>
2025-11-19 19:35:39 +0500

    create views about page and contacct page  after home page  and add urls in urls.py file

--- product/views.py
+++ product/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import View
 
 
 """A View is just a function or class that receives an HTTP request and returns a response.
@@ -15,11 +14,29 @@ from django.views import View
 def home():
     return {"meassage": "Welcome to Homepage!"}
 
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello From View!</h1>")
+
+# Change Default page to Custom Page:
+
+def home_view(request):
+    # print(request)
+    return HttpResponse("<h1> Hello From Home! </h1>")
+
+
+def contact_view(request):
+    return HttpResponse("<h1> Welcome to Contact Page!</h1>")
+   
+def about_view(reuest):
+    return HttpResponse("<h1> Welcome to About Page!</h1>")   
+
+
+
+
+
+
+
 # CBV = Views written using Python classes instead of functions.
 
-# def HomeView():
-#     def get(self, request):
-#         return HttpResponse("This is GET request")
+def HomeView():
+    def get(self, request):
+        return HttpResponse("This is GET request")
     
